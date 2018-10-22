9ab438748f99a39bfcadeeb1bddc9f375db0e731
jorgehrique <jorgek12o@hotmail.com>
2018-10-22 18:31:21 -0200

    calc and testes

--- src/calc.js
+++ src/calc.js
@@ -1,7 +1,14 @@
 module.exports = {
-    sum: function (a, b) { return a + b; },
-    sub: function (a, b) { return a - b; },
-    mult: function (a, b) { return a * b; },
-    div: function (a, b) { return (b === 0) ? 'Não é possível dividir por zero!' : a / b; },
-
+    sum: function (a, b) {
+        return a + b;
+    },
+    sub: function (a, b) {
+        return a - b;
+    },
+    mult: function (a, b) {
+        return a * b;
+    },
+    div: function (a, b) {
+        return (b === 0) ? 'Não é possível dividir por zero!' : a / b;
+    }
 }
